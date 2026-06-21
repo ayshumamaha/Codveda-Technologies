61fed3950e3ea6388f5fb7ff6af5dbc314ff1f7d
M. Ayshwarya <143439762+ayshumamaha@users.noreply.github.com>
2026-06-21 22:20:24 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,26 @@
+import random
+
+secret_number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to the Number Guessing Game")
+print("Guess a number between 1 and 100")
+
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too Low")
+        elif guess > secret_number:
+            print("Too High")
+        else:
+            print("Congratulations!")
+            print("You guessed the number in", attempts, "attempts")
+            break
+
+    except ValueError:
+        print("Please enter a valid number")
+
+
